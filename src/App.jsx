21f8aae194783d3289e92385cf478dabd22b0277
rbslm <rymbensalem816@gmail.com>
2025-04-30 02:03:02 -0400

    story section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
+import Story from "./components/Story";
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
     </main>
   );
 };
