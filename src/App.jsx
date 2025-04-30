a2ecb2715d422bee95f67c3b730fb1017542ee09
rbslm <rymbensalem816@gmail.com>
2025-04-30 01:42:44 -0400

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-violet-300 font-bold">
+        Welcome to Awwwards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
